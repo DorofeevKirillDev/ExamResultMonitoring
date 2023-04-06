ce3f992a508e2f8ddd8ae8722df5889c1e4dce52
DorofeevKirillDev <116457973+DorofeevKirillDev@users.noreply.github.com>
2023-04-06 11:57:06 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,16 @@
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        SitePageUpdater updater1 = new SitePageUpdater("http://mph1.phys.spbu.ru/~ivanov/index.html");
-        SitePageUpdater updater2 = new SitePageUpdater("http://mph1.phys.spbu.ru/~ivanov/3kypc/Course3.html");
+        SitePageUpdater updater = new SitePageUpdater("http://mph1.phys.spbu.ru/~ivanov/index.html");
         int counter = 1;
-        String recipients = "kirigdbchb@gmail.com, polina.teslina@yandex.ru";
-        String message = """
-                    Check your exam results""";
+        String recipients = "recip@gmail.com";
+        String message = "mesasge";
         MailNotificator mailNotificator = new MailNotificator();
         mailNotificator.setRamblerSMTPProperties();
-        mailNotificator.authenticate("claude.vidal.hx4mb@rambler.ru", "cm6Gk7SGi7p9");
+        mailNotificator.authenticate("login", "password");
         TelegramNotificator telegramNotificator = new TelegramNotificator();
-        //telegramNotificator.sendNotification("260682954", "test");
         while(true) {
-            updater1.checkForUpdates(new Notificator[] {mailNotificator, telegramNotificator}, recipients, message);
-            updater2.checkForUpdates(new Notificator[] {mailNotificator, telegramNotificator}, recipients, message);
-            //updater.checkForUpdates(telegramNotificator, recipients, message);
+            updater.checkForUpdates(new Notificator[] {mailNotificator, telegramNotificator}, recipients, message);
             Thread.sleep(60000);
             System.out.println("iterations: " + counter++);
         }
