53eb5209e3c150dd79408f23fe26893abe398d55
DorofeevKirillDev <116457973+DorofeevKirillDev@users.noreply.github.com>
2023-04-06 12:18:58 +0300

    Updated TelegramNotificator constructor

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,16 @@
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        SitePageUpdater updater = new SitePageUpdater("http://mph1.phys.spbu.ru/~ivanov/index.html");
-        int counter = 1;
+        SitePageUpdater updater = new SitePageUpdater("");
         String recipients = "recip@gmail.com";
         String message = "mesasge";
         MailNotificator mailNotificator = new MailNotificator();
         mailNotificator.setRamblerSMTPProperties();
         mailNotificator.authenticate("login", "password");
-        TelegramNotificator telegramNotificator = new TelegramNotificator();
+        TelegramNotificator telegramNotificator = new TelegramNotificator("BOT_TOKEN");
         while(true) {
             updater.checkForUpdates(new Notificator[] {mailNotificator, telegramNotificator}, recipients, message);
             Thread.sleep(60000);
-            System.out.println("iterations: " + counter++);
         }
     }
 }
