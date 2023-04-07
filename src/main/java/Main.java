ae0dc71603a2def683d52bc78dda697212403ca4
DorofeevKirillDev <116457973+DorofeevKirillDev@users.noreply.github.com>
2023-04-07 22:39:33 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,13 @@
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        SitePageUpdater updater = new SitePageUpdater("");
+        SitePageUpdater updater = new SitePageUpdater("url.com");
         String recipients = "recip@gmail.com";
         String message = "mesasge";
         MailNotificator mailNotificator = new MailNotificator();
         mailNotificator.setRamblerSMTPProperties();
         mailNotificator.authenticate("login", "password");
-        TelegramNotificator telegramNotificator = new TelegramNotificator("BOT_TOKEN");
+        TelegramNotificator telegramNotificator = new TelegramNotificator("botToken", "botName");
         while(true) {
             updater.checkForUpdates(new Notificator[] {mailNotificator, telegramNotificator}, recipients, message);
             Thread.sleep(60000);
